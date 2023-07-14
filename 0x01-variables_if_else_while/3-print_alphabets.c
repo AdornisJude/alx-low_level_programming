fd0357f962a2d4a3c668025856a94a195b1404dd
AdornisJude <Jude_Adornis@yahoo.com>
2023-07-14 07:31:56 -0700

    Alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,27 +1,26 @@
 /*
- * File: 3-print_alphabets.c
- * Author: Jude M
- */
+*File: 3-print_alphabets.c
+*Auth: Jude M
+*/
 
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase, and then in uppercase.
- *
- * Return: Always (0)
- */
-
+* main - Prints the alphabet in lowercase, and then in uppercase.
+*
+*Return: Always 0.
+*/
 int main(void)
 {
 	char c;
 
-	(c = 'a');
-	while
-	(c <= 'z');
-	{
+	for (c = 'a'; c <= 'z'; c++)
 	putchar(c);
-	c++;
-	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	putchar(c);
+
 	putchar('\n');
+
 	return (0);
 }
