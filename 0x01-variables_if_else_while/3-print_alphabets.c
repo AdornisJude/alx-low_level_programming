df357801cd7467360a7ce8a8a6dd18078f3b5bf7
AdornisJude <Jude_Adornis@yahoo.com>
2023-07-14 07:15:22 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+/*
+ * File: 3-print_alphabets.c
+ * Author: Jude M
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	char c;
+
+	(c = 'a');
+	while
+	(c <= 'z');
+	{
+	putchar(c);
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
