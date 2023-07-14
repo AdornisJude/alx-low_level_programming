a769f9ed5022fb4285c684952b9d2a6b14cddfd0
AdornisJude <Jude_Adornis@yahoo.com>
2023-07-14 06:49:59 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * File_name: 2-print_alphabet.c
+ * Auth:Jude M
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
