da8b5f3ab4ec51fdf341d455d53ea79cd970dec3
AdornisJude <Jude_Adornis@yahoo.com>
2023-07-18 08:42:00 -0700

    Update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,27 @@
-/**
-*main - Prints _putchar followed by a new line.
-*
-*Return: 0
-*/
+/*
+ * File: 0-putchar.c
+ * Auth: Jude
+ *
+ */
 
+#include "main.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
