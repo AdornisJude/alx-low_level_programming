b853d296d4f6f3c07594d0ce8b4a61535719045b
AdornisJude <Jude_Adornis@yahoo.com>
2023-07-20 15:25:06 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 
-/**
-* main - causes and infinte loop
-* Return: 0
-*/
+void positive_or_negative(int i)
+int largest_number(int a, int b, int c)
 
-int main(void)
-{
-	int i;
-
-	printf("infinte loop incoming :(\n")
-
-	i = 0
-
-	/*while (i < 10)*/
-	/*{*/
-	/* putchar(i);*/
-	/*}*/
-
-	printf("Infinite loop avaoided! \\o\n");
-
-	return (0);
-}
+#endif /*MAIN_H */
