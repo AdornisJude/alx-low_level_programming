39005de068f172cc1661ef2f7eb891fdb31f33c3
AdornisJude <Jude_Adornis@yahoo.com>
2023-07-24 16:36:01 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,6 +3,7 @@
 /**
  * puts_half - prints half of a string
  * followed by a new line
+ * @str: string to be printed
  */
 void puts_half(char *str)
 {
@@ -12,21 +13,21 @@ void puts_half(char *str)
 
 	while (str[len] != '\0')
 	{
-	len++;
+		len++;
 	}
 
 	if (len % 2 == 0)
 	{
-	for (i = len / 2; str[i] != '\0'; i++)
-	{
-	_putchar(str[i]);
-	}
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
 	} else if (len % 2)
 	{
-	for (n = (len - 1) / 2; n < len - 1; n++)
-	{
-	_putchar(str[n + 1]);
-	}
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
 	}
 	_putchar('\n');
 }
