a372c77519696dc5f60578991ddcd56042322b5d
AdornisJude <Jude_Adornis@yahoo.com>
2023-07-24 17:06:48 -0700

    Advanced Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 16
+
+int main(void) 
+{
+	char password[PASSWORD_LENGTH + 1];
+	int i;
+
+	srand(time(NULL));
+
+	for (i = 0; i < PASSWORD_LENGTH; i++) 
+	{
+		password[i] = 'A' + (rand() % 26);
+	}
+
+	password[PASSWORD_LENGTH] = '\0';
+
+	printf("Generated password: %s\n", password);
+
+	return 0;
+}
