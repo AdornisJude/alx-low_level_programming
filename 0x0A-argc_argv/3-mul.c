b1c464b7d55db727211cae889a9c004dfdc3aea3
AdornisJude <Jude_Adornis@yahoo.com>
2023-08-04 14:15:35 -0700

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main -  This helps to print the product of two numbers
+* @argc: Counts the number of arguments used
+* @argv: This represents the argument vector
+* Return: This returns 0 always
+*/
+
+int main(int argc, char *argv[])
+{
+	int First_Number, Second_Number, Prod = 0;
+	
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		First_Number = atoi(argv[1]);
+		Second_Number = atoi(argv[2]);
+
+		Prod = First_Number * Second_Number;
+		printf("%d\n", Prod);
+	}
+	return (0);
+}
