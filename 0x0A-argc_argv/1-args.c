1e9f47bbb194f5e70a1a750d63fd015ccae591e7
AdornisJude <Jude_Adornis@yahoo.com>
2023-08-04 13:23:54 -0700

    Task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,13 +4,13 @@
 * main - This prints the number of arguments
 * @argc: Counts the number of arguments
 * @argv: this is the argument  vector
-* Return: Always returns 0
+* Return: returns 0
 */
 
 int main(int argc, char *argv[])
 {
 	(void) argv;
 
-	printf("%d\n", argc -1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
